25dc387d4e0d656d0740c62290fc75f6680cad3e
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-15 17:42:17 +0700

    Week08P03

--- Week08/Week08/Program.cs
+++ Week08/Week08/Program.cs
@@ -11,15 +11,20 @@ namespace Week08
         static void Main(string[] args)
         {
             int[] data = new int[10];
-            data[-1] = 0;
-            data[1] = 1;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[-1]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            Student[] student = new Student[31]; // สร้างอาร์เรย์ของวัตถุจากคลาส
+            Console.WriteLine(data.Length);
+            Console.WriteLine(student.Length);
             Console.ReadLine();
+        }
+        class Student     // nested class
+        {
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+
+            }
         }
     }
 }
