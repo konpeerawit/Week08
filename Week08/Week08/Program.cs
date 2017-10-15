f0e41b296f5bd0cd7899bdf58b700f56d94d79f5
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-15 20:41:23 +0700

    Week08P14

--- Week08/Week08/Program.cs
+++ Week08/Week08/Program.cs
@@ -11,25 +11,22 @@ namespace Week08
     {
         static void Main(string[] args)
         {
-            Queue<string> queue1 = new Queue<string>();
-            queue1.Enqueue("Red");
-            queue1.Enqueue("Green");
-            queue1.Enqueue("Blue");
-            queue1.Enqueue("Orange");
-            queue1.Enqueue("Yellow");
-            Console.WriteLine("The elements in the queue are:");
-            foreach (string s in queue1)
+            Queue<int> queue1 = new Queue<int>();
+            queue1.Enqueue(10);
+            queue1.Enqueue(20);
+            queue1.Enqueue(30);
+            queue1.Enqueue(40);
+            Console.WriteLine("The queue elements are:");
+            foreach (int i in queue1)
             {
-                Console.WriteLine(s);
+                Console.WriteLine(i);
             }
-            Console.WriteLine("The element Red is contain in the queue:"
-            + queue1.Contains("Red"));
-            Console.WriteLine("The element Black is contain in the queue:"
-            + queue1.Contains("Black"));
-            Console.WriteLine("The element Purple is contain in the queue:"
-            + queue1.Contains("Purple"));
-            Console.WriteLine("The element Green is contain in the queue:"
-            + queue1.Contains("Green"));
+            ArrayList array = new ArrayList(queue1.ToArray());
+            Console.WriteLine("\nContents of the copy");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadLine();
 
         }
