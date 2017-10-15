1f7573fd6b453f55928cbd133513d77b5a6ba9fc
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-15 18:16:38 +0700

    Week08P06E01

--- Week08/Week08/Program.cs
+++ Week08/Week08/Program.cs
@@ -10,46 +10,22 @@ namespace Week08
     {
         static void Main(string[] args)
         {
-            // Array of characters.
-            char[] array1 = { 'q', 'w', 'e', 'r', 't',
-                'y', 'u', 'i', 'o', 'p' };
-            Array.Sort(array1);
-
-            // Array of strings.
-            string[] colors = new string[] { "red",
-              "orange", "blue", "green", "yellow", "lemon", "aqua"  };
-            Array.Sort(colors);
-
-            string[] AseanCountries = new string[] { "Cambodia", "Malaysia",
-              "Indonesia", "Singapore", "Thailand", "Philippines",
-              "Vietnam", "Brunei Darussalam", "Laos", "Myanmar" };
-
+            string[] monthsName = new string[]
+    { "January","February","March","April","May","June","July","August", "September", "October", "November",  "December" };
             // C# program that uses LINQ
-            var sortAscending = from country in AseanCountries
-                                orderby country
-                                select country;
-            var sortDescending = from country in AseanCountries
-                                 orderby country descending
-                                 select country;
-
-            // print output
-            Console.WriteLine("------Character sorting----------");
-            foreach (var c in array1)
-                Console.WriteLine(c);
-
-            Console.WriteLine("------String sorting----------");
-            foreach (string color in colors)
-                Console.WriteLine(color);
-
+            var sortAscending = from months in monthsName
+                                orderby months
+                                select months;
+            var sortDescending = from months in monthsName
+                                 orderby months descending
+                                 select months;
             Console.WriteLine("------String sort ascending----------");
             foreach (string c in sortAscending)
                 Console.WriteLine(c);
-
             Console.WriteLine("------String sort descending----------");
             foreach (string c in sortDescending)
                 Console.WriteLine(c);
-            // wait
-            Console.ReadLine();
+            Console.ReadKey();
 
         }
     }
